8169ce1c904874309267a87c6cc486a70ab38a42
Mharlee Dela Cruz <marwilsondc1204@gmail.com>
2025-05-20 01:52:54 +0800

    added class Corrects, added initial class code, and imported classes into quiz_creator_2.py

--- quiz_creator/classes/Corrects.py
+++ quiz_creator/classes/Corrects.py
@@ -1,16 +1,16 @@
-class Choices: 
+class Corrects:
     
     def __init__(self, content, code):
         self.content = content
         self.code = code
-
-    #to return choice as string
+    
+    #to return correct answer objects as strings
     def __str__(self):
         return f"{self.content}"
     
-    #to write choice in file
+    #to write correct answers in file
     def write(self, path):
         new_line = "\n"
 
         with open(path, "a") as file:
-            file.write(f"<{self.code:b}> <choice>:{self.content}{new_line}")
+            file.write(f"<{self.code:b}> <correct>:{self.content}{new_line}")
--- quiz_creator/quiz_creator_2.py
+++ quiz_creator/quiz_creator_2.py
@@ -1,4 +1,11 @@
 #import pathlib
+import pathlib 
+
 #import Questions
+from classes.Questions import Questions
+
 #import Choices
+from classes.Choices import Choices
+
 #import Corrects
+from classes.Corrects import Corrects
